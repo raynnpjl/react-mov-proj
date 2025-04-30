8fe4e6736c9d740153f8abde7f48be8e2ecaf861
raynnpjl <101748970+raynnpjl@users.noreply.github.com>
2025-05-01 02:41:19 +0800

    [FINAL] Completed Favorites Feature & Pages

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,22 @@
 import "../css/MovieCard.css"
+import { useMovieContext } from "../contexts/MovieContext"
 
 function MovieCard({movie}) {
+    const { isFavorite, addToFavorites, removeFromFavorites } = useMovieContext();
+    const favorite = isFavorite(movie.id);
 
-    function onFavClick() {
-        alert("clicked")
+
+    function onFavClick(e) {
+        e.preventDefault()
+        if (favorite) removeFromFavorites(movie.id)
+            else addToFavorites(movie)
     }
 
     return <div className="movie-card">
         <div className="movie-poster">
             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
             <div className="movie-overlay">
-                <button className="favorite-btn" onCLick={onFavClick}>
+                <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavClick}>
                     ♥
                 </button>
             </div>
