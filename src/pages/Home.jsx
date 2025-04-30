06dee6e0433ef362efe8a3f3e1439054b1e50d0a
raynnpjl <101748970+raynnpjl@users.noreply.github.com>
2025-04-30 22:23:25 +0800

    [UPDATE} Page Routing - Home, Favourites w/ NavBar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import MovieCard from "../components/MovieCard"
+import {useState} from "react"
+
+function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const movies = [
+        {id: 1, title: "John Wick", release_date: "2020"},
+        {id: 2, title: "Terminator", release_date: "2024"},
+        {id: 3, title: "Matrix", release_date: "2021"},
+    ];
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("")
+    };
+
+    return (
+    <div className="home">
+        <form onSubmit={handleSearch} className="search-form">
+            <input 
+                type="text" 
+                placeholder="Search for movies..." 
+                className="search-input"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button type="submit" className="search-button">Search</button>
+        </form>
+
+        <div className="movies-grid">
+            {movies.map(movie => (
+                (
+                <MovieCard movie={movie} key={movie.id} />
+                )
+            ))}
+        </div>
+    </div>
+    );
+}
+
+export default Home
